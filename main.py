d078a396cdb330c7375f80677905a279a3ea636a
Kaye Feinberg <feincrafts@gmail.com>
2024-03-08 18:19:50 -0500

    basic flask calculator

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def default():
+    return "Hello!"
+
+@app.route('/add/<num1>/<num2>')
+def add_nums(num1,num2):
+    return str(int(int(num1) + int(num2)))
+
+@app.route('/subtract/<num1>/<num2>')
+def subtract_nums(num1,num2):
+    return str(int(int(num1) - int(num2)))
+
+@app.route('/multiply/<num1>/<num2>')
+def multiply_nums(num1,num2):
+    return str(int(int(num1) * int(num2)))
+
+@app.route('/divide/<num1>/<num2>')
+def divide_nums(num1,num2):
+    if num2 == 0:
+        return "Invalid operation, can't divide by zero"
+    else:
+        return str(round((int(num1) / int(num2)), 2))
